61662aa8a31c78031b0b3a0e48a45607239c5e6b
nhogg <nathanhogg1223@gmail.com>
2024-10-03 16:28:39 -0500

    TrainLine Done

--- 05-redline/TrainLine.java
+++ 05-redline/TrainLine.java
@@ -67,30 +67,49 @@ public class TrainLine {
         return numberOfStations;
     } // method getNumberOfStations
 
-
+    /**
+     * Checks if given String is present in the TrainStation object by traversing the list
+     * and performing a check for each station. Safe operation space is created in
+     * the while loop evaluating this.isEmpty to check if TrainLine is empty.
+     * If the name is either null or does not exist in the TrainLine, False is returned.
+     *
+     * @param name String: name to search for
+     * @return found: boolean. True if station found, False if not.
+     */
     public boolean contains(String name) {
         boolean found = false;
 
         int i = 0;
         TrainStation cursor = this.head;
-        while (i < this.getNumberOfStations() && !found && cursor != null) {
+        while (i < this.getNumberOfStations() && !found && !this.isEmpty()) {
             if (cursor.getName().equals(name)) {
                 found = true;
             }
             cursor = cursor.getNext();
             i++;
 
-
         }
         return found;
-    }
+    } // methodContains
 
+    /**
+     * Given a name to search for, returns the index of that station by traversing
+     * the TrainLine and checking if the cursor (initialized to this.head to start
+     * at the first station) equals the desired name.
+     * Safe operation space is created in
+     * the while loop by Safe operation space is created in
+     * the while loop evaluating this.isEmpty to check if TrainLine is empty.
+     * Index is initialized to -1, so if the name is null or not present, -1 is returned.
+     *
+     * @param name String: name of station to search for
+     * @return int: the index of the station
+     */
     public int indexOf(String name) {
         int index = -1;
 
         int i = 0;
         TrainStation cursor = this.head;
-        while (i < this.getNumberOfStations() && cursor != null) {
+        while (i < this.getNumberOfStations() && !this.isEmpty()) {
             if (cursor.getName().equals(name)) {
                 index = i;
             }
@@ -99,28 +118,70 @@ public class TrainLine {
 
         }
         return index;
-    }
+    } // method indexOf
 
+    /**
+     * Reverses the TrainLine and returns a String representing the names
+     * of each station in reverse order. This is achieved by creating a
+     * temporary list (reverseHead) that stores the stations in their
+     * reverse order. We traverse the original linked list, creating
+     * a new TrainStation object for each station. My original implementation
+     * (commented out) used two for loops, iterating through the list
+     * front to back and back to front.
+     * Safe operation space is created by evaluating if !this.isEmpty().
+     * If it is not, the rest of the code executes. If it is null, "" is returned
+     * to signify that the TrainLine is empty.
+     *
+     * @return String: reversed list
+     */
     public String reverseList() {
-        String list = ""; // If trainLine has no stations, return empty String
-        if (this.head != null) {
-            // Iterate trainLine backwards
-            for (int i = this.getNumberOfStations() - 1; i >= 0; i--) {
-                TrainStation cur = this.head;
-                // Traverse to the ith station
-                for (int j = 0; j < i; j++) {
-                    cur = cur.getNext(); // Move to the next station
-                }
-                list += (cur.getName() + "\n"); // Add station to String
+        // Original implementation using 2 for loops (bad)
+//        String list = ""; // If trainLine has no stations, return empty String
+//        if (this.head != null) {
+//            // Iterate trainLine backwards
+//            for (int i = this.getNumberOfStations() - 1; i >= 0; i--) {
+//                TrainStation cur = this.head;
+//                // Traverse to the ith station
+//                for (int j = 0; j < i; j++) {
+//                    cur = cur.getNext(); // Move to the next station
+//                }
+//                list += (cur.getName() + "\n"); // Add station to String
+//            }
+//        }
+//
+//        return list;
+        // New implementation using 2 using 2 while loops (slightly better)
+        String list = "";
+        if (!this.isEmpty()) {
+            TrainStation cursor = this.head;
+
+            TrainStation reverseHead = null;
+
+            while (cursor != null) {
+                TrainStation newStation = new TrainStation(cursor.getName());
+                newStation.setNext(reverseHead);
+                reverseHead = newStation;
+                cursor = cursor.getNext();
+            }
+
+            TrainStation reverseCursor = reverseHead;
+
+            while (reverseCursor != null) {
+                list += reverseCursor.getName() + "\n";
+                reverseCursor = reverseCursor.getNext();
+
             }
         }
 
         return list;
-    }
+    } // method reverseList
 
+    /**
+     * @return boolean: True if TrainStation is empty, False if it is not.
+     */
     public boolean isEmpty() {
         return this.head == null;
-    }
+    } // method isEmpty
 
     /*******************************************************************************
      * DO NOT REMOVE TESTS FROM THE CODE BELOW. YOU MAY **ADD** YOUR OWN TESTS BUT *
@@ -128,8 +189,7 @@ public class TrainLine {
      ******************************************************************************/
     public static void main(String[] args) {
         // A few station names
-        String[] stationNames = {"Howard", "Jarvis", "Morse",
-                "Loyola", "Granville", "Thorndale"};
+        String[] stationNames = {"Howard", "Jarvis", "Morse", "Loyola", "Granville", "Thorndale"};
         // A populated trainline
         TrainLine redLineSB = new TrainLine("Red Line SB");
         for (String station : stationNames) {
@@ -186,18 +246,17 @@ public class TrainLine {
         System.out.printf(formatContainsTestNonExisting, reportContainsTestNonExisting);
         System.out.printf(formatReverseListTest, reportReverseListTest);
         // ----------- YOU MAY ADD YOUR OWN TESTS BELOW THIS COMMENT LINE ---------------
+        String[] names = {"one", "two", "three", "four"};
         TrainLine nathansTrain = new TrainLine("Nathans Train");
-        nathansTrain.add("One");
-        nathansTrain.add("Two");
-        nathansTrain.add("Three");
-        System.out.println(nathansTrain.contains("Two"));
-        System.out.println(nathansTrain.indexOf("Two"));
-        System.out.println();
-        TrainLine empty = new TrainLine("Empty");
-        System.out.println(empty.isEmpty());
-        empty.add("One");
-        System.out.println(empty.isEmpty());
+        for (String s : names) {
+            nathansTrain.add(s);
+        }
+        System.out.println(nathansTrain.reverseList());
         System.out.println(redLineSB.reverseList());
-        System.out.println(redLineSB.isEmpty());
+        TrainStation cursor = nathansTrain.head;
+        for (int i = 0; i < nathansTrain.numberOfStations; i++) {
+            System.out.println(cursor.getName());
+            cursor = cursor.getNext();
+        }
     } // method main
 } // class TrainLine
