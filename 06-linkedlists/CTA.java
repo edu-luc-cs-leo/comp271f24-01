d74165c156d0b8c0a4848e5fcb9394431fc3c5f1
nhogg <nathanhogg1223@gmail.com>
2024-10-14 11:36:29 -0500

    Added CTA class

---
+++ 06-linkedlists/CTA.java
@@ -0,0 +1,13 @@
+public class CTA {
+
+    public static final String[] RED_LINE_SB_NAMES = {
+            "Howard", "Jarvis", "Morse", "Loyola", "Granville",
+            "Thorndale", "Bryn Mawr", "Berwyn", "Argyle", "Lawrence",
+            "Wilson", "Sheridan", "Addison", "Belmont", "Fullerton",
+            "North/Clybourn", "Clark/Division", "Chicago", "Grand", "Lake",
+            "Monroe", "Jackson", "Harrison", "Roosevelt", "Cermak-Chinatown",
+            "Sox-35th", "47th", "Garfield", "63rd", "69th",
+            "79th", "87th", "95th/Dan Ryan"
+    };
+
+}
